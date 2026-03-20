7d79ace505ba195f569c9ddf622b5be918d738aa
rbaezseguros-coder <rbaez.seguros@gmail.com>
2026-03-20 02:21:37 -0300

    Update procesar.js

--- netlify/functions/procesar.js
+++ netlify/functions/procesar.js
@@ -1,4 +1,4 @@
-const { GoogleGenerativeAI } = require("@google-generative-ai/generative-ai");
+const { GoogleGenerativeAI } = require("@google/generative-ai");
 
 exports.handler = async (event) => {
   // 1. Verificamos que sea un pedido POST
